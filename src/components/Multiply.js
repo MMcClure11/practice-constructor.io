09f5ab35bba2eddd35b4b6a4fb91c16ad8236fa7
MMcClure11 <mcclme02@luther.edu>
2021-03-15 17:23:19 -0500

    Add multiply function

--- src/components/Multiply.js
+++ src/components/Multiply.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Multiply = () => {
+  const [product, setProduct] = useState(0)
+
+  const multiply = (num) => {
+    return (num2) => num * num2
+  }
+  
+  const onClickHandler = () => {
+    setProduct( multiply(5)(6) )
+  }
+
   return (
     <div>
-      Jordan's multiply test
+      <h2>Jordan's Skilled Multiply Test</h2>
+      <p>How can you make the function multiply(5)(6) return 30?</p>
+      <button onClick={ onClickHandler }>Multiply</button>
+      <h1>Product: { product }</h1>
     </div>
   )
 }
