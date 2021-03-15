b8f0ee7e580dd9eb918e8445c5992d9ef0ced68d
MMcClure11 <mcclme02@luther.edu>
2021-03-15 14:51:06 -0500

    Add state for fizzBuzz, show FizzBuzz when the counter is divisible by 3 and 5

--- src/components/FizzBuzz.js
+++ src/components/FizzBuzz.js
@@ -3,16 +3,30 @@ import React, { useState } from 'react'
 const FizzBuzz = () => {
 
   const [counter, setCounter] = useState(0)
+  const [fizzBuzz, setFizzBuzz] = useState('')
 
   const onClickHandler = () => {
+    if( (counter + 1) % 5 === 0 && (counter + 1) % 3 === 0 ) {
+      setFizzBuzz('FizzBuzz')
+      setCounter(prevCount => prevCount + 1)
+    } else {
+    setFizzBuzz('')
     setCounter(prevCount => prevCount + 1)
+    }
   }
+
+  console.log(counter, fizzBuzz)
 
   return (
     <div>
       <h1>FizzBuzz</h1>
       <h3>Click on the counter to see FizzBuzz in action!</h3>
-      <h2> Count: { counter }</h2>
+      { fizzBuzz !== '' 
+      ? 
+      <h2>Count: { fizzBuzz }</h2>
+      :
+      <h2>Count: { counter }</h2>
+      }
       <button onClick={ onClickHandler }>Counter</button>
     </div>
   )
