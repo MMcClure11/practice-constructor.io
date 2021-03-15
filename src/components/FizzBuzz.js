c43dfa83e35ed88c232f9015c1774b728fb8ec8a
MMcClure11 <mcclme02@luther.edu>
2021-03-15 14:41:08 -0500

    Add state and button to increase count for fizzbuzz

--- src/components/FizzBuzz.js
+++ src/components/FizzBuzz.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FizzBuzz = () => {
+
+  const [counter, setCounter] = useState(0)
+
+  const onClickHandler = () => {
+    setCounter(counter + 1)
+  }
+
   return (
     <div>
       I am the fizz buzz component
+      <h2> Count: { counter }</h2>
+      <button onClick={ onClickHandler }>Counter</button>
     </div>
   )
 }
