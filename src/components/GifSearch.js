2153342d460ee38534075bc133aca8e352f92023
MMcClure11 <mcclme02@luther.edu>
2021-03-17 17:48:31 -0500

    Add controlled form for search

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -1,14 +1,23 @@
-import React from 'react'
-
-const searchHandler = () => {
-  alert('searched!')
-}
+import React, { useState } from 'react'
 
 const GifSearch = () => {
+
+  const [ query, setQuery ] = useState('')
+
+  const searchHandler = (e) => {
+    e.preventDefault()
+    alert('searched!')
+  }
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target
+    setQuery(value)
+  }
+
   return (
     <div>
       <form onSubmit={ searchHandler }>
-        <input type='text' placeholder='search for a gif' />
+        <input type='text' placeholder='search for a gif' onChange={ onChangeHandler } name='query' value={ query } />
         <input type='submit' name='search' value='Search' />
       </form>
     </div>
