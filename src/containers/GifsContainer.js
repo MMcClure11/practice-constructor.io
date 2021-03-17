19dbf8e7a8c95f7a586d56d0616a04e1af32b38b
MMcClure11 <mcclme02@luther.edu>
2021-03-17 18:02:16 -0500

    Set state with the returned array of data

--- src/containers/GifsContainer.js
+++ src/containers/GifsContainer.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GifSearch from '../components/GifSearch'
 import GifsList from './GifsList'
 
 const GifsContainer = () => {
+
+  const [gifs, setGifs] = useState([])
   
   const setGifsHandler = (query) => {
     return fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=COI788NHznUTLid1ciKLW2cPSQ4voDn1`)
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => setGifs(data.data))
   }
+
+  console.log(gifs)
   return (
     <div>
-      <h1>I'm the gifs container</h1>
       <GifSearch setGifsHandler={ setGifsHandler } />
       <GifsList />
     </div>
