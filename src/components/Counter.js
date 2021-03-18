2a8c38d88b8e91033026403deefeff2c66e905e0
MMcClure11 <mcclme02@luther.edu>
2021-03-18 13:51:38 -0500

    Add pauseCounter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PauseCounter from './PauseCounter';
 
 const Counter = () => {
 
@@ -40,25 +41,25 @@ const Counter = () => {
     setCounter(prevCount => prevCount - 1)
   }
 
-  const [pausableCounter, setPausableCounter] = useState(0)
+  // const [pausableCounter, setPausableCounter] = useState(0)
 
-  const increasePausableCounter = () => {
-    setPausableCounter(prevCount => prevCount + 1)
-  }
+  // const increasePausableCounter = () => {
+  //   setPausableCounter(prevCount => prevCount + 1)
+  // }
 
- const start = () => {
-   const interval = setInterval(() => {
-     increasePausableCounter()
-   }, 1000)
- }
+//  const start = () => {
+//    const interval = setInterval(() => {
+//      increasePausableCounter()
+//    }, 1000)
+//  }
 
 // const pause = () => {
 //   clearInterval(interval)
 // }  
 
-const resetPausableCounter = () => {
-  setPausableCounter(0)
-} 
+// const resetPausableCounter = () => {
+//   setPausableCounter(0)
+// } 
 
   return (
     <div>
@@ -74,10 +75,11 @@ const resetPausableCounter = () => {
       <button onClick={ decreaseCounter }>-</button>
       <button onClick={ resetCounter }>0</button>
       <h1>I am a counter you can start and stop.</h1>
-      <h2>Count: { pausableCounter }</h2>
-      <button onClick={ start }>Start</button>
+      {/* <h2>Count: { pausableCounter }</h2> */}
+      <PauseCounter />
+      {/* <button onClick={ start }>Start</button> */}
       {/* <button onClick={ pause }>Pause</button> */}
-      <button onClick={ resetPausableCounter }>0</button>
+      {/* <button onClick={ resetPausableCounter }>0</button> */}
     </div>
   )
 }
---
+++ src/components/PauseCounter.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class PauseCounter extends Component {
+
+  render(){
+    return(
+      <div>
+        I am the pause counter component
+      </div>
+    )
+  }
+
+}
+
+export default PauseCounter
