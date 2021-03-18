32d8ae2295e6fde0a49e092c04e3f522c6204889
MMcClure11 <mcclme02@luther.edu>
2021-03-17 19:03:03 -0500

    Pass query up from SearchGif and down to GifsList

--- src/containers/GifsContainer.js
+++ src/containers/GifsContainer.js
@@ -5,8 +5,10 @@ import GifsList from './GifsList'
 const GifsContainer = () => {
 
   const [gifs, setGifs] = useState([])
+  const [query, setQuery] = useState('')
   
   const setGifsHandler = (query) => {
+    setQuery(query)
     return fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=COI788NHznUTLid1ciKLW2cPSQ4voDn1`)
       .then(res => res.json())
       .then(data => setGifs(data.data))
@@ -15,7 +17,7 @@ const GifsContainer = () => {
   return (
     <div>
       <GifSearch setGifsHandler={ setGifsHandler } />
-      <GifsList gifs={ gifs } />
+      <GifsList gifs={ gifs } query={ query } />
     </div>
   )
 }
--- src/containers/GifsList.js
+++ src/containers/GifsList.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import GifCard from '../components/GifCard'
 
-const GifsList = ({ gifs }) => {
+const GifsList = ({ gifs, query }) => {
 
   const renderGifs = () => {
     return gifs.map(gif => <GifCard key={ gif.id } gif={ gif } />)
   }
-  console.log(gifs)
+  console.log(query)
   return (
     <div>
       <h3>Check out these cool gifs your search returned!</h3>
