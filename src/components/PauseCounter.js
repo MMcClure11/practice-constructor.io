5be7ce26f9f1c6e5fae1438f2005139ee59fcf9a
MMcClure11 <mcclme02@luther.edu>
2021-03-18 13:54:12 -0500

    Add constructor with super props to PauseCounter

--- src/components/PauseCounter.js
+++ src/components/PauseCounter.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react'
 
 class PauseCounter extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
   render(){
     return(
       <div>
         I am the pause counter component
+        <h2>Count: { this.state.count }</h2>
       </div>
     )
   }
