0319aeea7c68eefb4422fcb313f0392322b67a53
MMcClure11 <mcclme02@luther.edu>
2021-03-21 14:53:23 -0500

    Add fetch to pokemon api

--- src/containers/PokemonContainer.js
+++ src/containers/PokemonContainer.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const PokemonContainer = () => {
+  const URL = 'https://pokeapi.co/api/v2/pokemon';
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+      });
+  }, []);
+
   return (
     <div>
       PokemonContainer
